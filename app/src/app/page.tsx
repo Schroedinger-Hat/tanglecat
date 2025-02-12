310bf2344715c4cad6ae2ac17083174d0d20a6d8
TheJoin95 <miki.lombi@gmail.com>
2025-02-12 18:09:10 +0100

    fix: welcome text

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
       <>
         <div className="flex flex-col items-center justify-center">
         <div className="w-full max-w-md px-4 py-8">
-            <h1 className="text-3xl font-bold text-center mb-8">Welcome to OSDay25</h1>
+            <h1 className="text-3xl text-white font-bold text-center mb-8 filter drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Welcome to OSDay25</h1>
             
             <div className="
                 w-full
