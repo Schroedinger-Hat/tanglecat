3f65b016a422a5488bf396aaeaad130aae53977a
TheJoin95 <miki.lombi@gmail.com>
2025-02-24 14:44:54 +0100

    fix(db): remove email template

--- app/schemaTypes/index.ts
+++ app/schemaTypes/index.ts
@@ -1,11 +1,9 @@
 import challenge from './challenge'
-import emailTemplate from './emailTemplate'
 import eventCode from './eventCode'
 import user from './user'
 import award from './award'
 export const schemaTypes = [
   challenge,
-  emailTemplate,
   eventCode,
   user,
   award
