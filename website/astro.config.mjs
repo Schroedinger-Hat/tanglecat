00602d180e82679b07ddf5547b7c4cac20ed5e42
TheJoin95 <miki.lombi@gmail.com>
2025-07-10 16:16:49 +0200

    feat(lottie): add cat animation

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,18 +3,22 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import starlight from '@astrojs/starlight'
+import lottie from 'astro-integration-lottie'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'Documentation',
-    logo: {
-      src: '/public/tanglecat-logo.png',
-      alt: 'TangleCat logo',
-      replacesTitle: false
-    },
-    customCss: ['./src/styles/global.css']
-  })],
+  integrations: [
+    starlight({
+      title: 'Documentation',
+      logo: {
+        src: '/public/tanglecat-logo.png',
+        alt: 'TangleCat logo',
+        replacesTitle: false
+      },
+      customCss: ['./src/styles/global.css']
+    }),
+    lottie()
+  ],
   vite: {
     plugins: [tailwindcss()]
   }
