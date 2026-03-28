d2df086d6cc25f272ea928e4cc43bba8191b290f
Patrick Raedler <patrick.raedler@schroedinger-hat.org>
2026-03-28 18:10:36 +0100

    chore(deps): update app and sanity dependencies

--- app/sanity.cli.ts
+++ app/sanity.cli.ts
@@ -1,13 +1,19 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from "sanity/cli"
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.SANITY_STUDIO_PROJECT_ID || process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'n9u23251',
-    dataset: process.env.SANITY_STUDIO_DATASET || process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+    projectId:
+      process.env.SANITY_STUDIO_PROJECT_ID ||
+      process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
+      "n9u23251",
+    dataset:
+      process.env.SANITY_STUDIO_DATASET ||
+      process.env.NEXT_PUBLIC_SANITY_DATASET ||
+      "production",
   },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
-  autoUpdates: true,
+  deployment: { autoUpdates: true },
 })
